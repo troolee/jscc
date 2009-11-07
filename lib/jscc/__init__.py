65a5b80e2bcb60ae95a0f284aa4e497179ec8631
Pavel Reznikov <pashka.reznikov@gmail.com>
2009-11-07 10:23:20 +0200

    Creating base project structure

---
+++ lib/jscc/__init__.py
@@ -0,0 +1,5 @@
+from manager import Manager
+
+def run(argv):
+    m = Manager()
+    m.run()
---
+++ lib/jscc/manager.py
@@ -0,0 +1,5 @@
+class Manager(object):
+    def run(self):
+        print ">>> jscc is wathing for changes. Press Ctrl-C to stop."
+
+    
