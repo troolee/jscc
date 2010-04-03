28f57985f8cc4000d171af78e52b78ed1f385280
Pavel Reznikov <pashka.reznikov@gmail.com>
2010-04-03 13:01:46 +0300

    Add --force and --debug-mode

--- lib/jscc/manager.py
+++ lib/jscc/manager.py
@@ -29,6 +29,8 @@ class Manager(object):
         parser.add_option_group(mode_group)
 
         parser.add_option('--compiler', default='closure-compiler', help='Path to the google closure compiler. By default used: closure-compiler (see INSTALL for more details)')
+        parser.add_option('-f', '--force', dest='force', action='store_true', help='Force recompile the project.')
+        parser.add_option('-D', '--debug-mode', dest='debug_mode', action='store_true', help='Do not compile files. Just concatinate them.')
         parser.add_option('-d', '--debug', action='store_true', help='Display debug output')
                 
         (options, args) = parser.parse_args(argv)
@@ -36,6 +38,8 @@ class Manager(object):
             parser.error("incorrect number of arguments")
 
         self.compiler = options.compiler
+        self.debug_mode = options.debug_mode or False
+        force = options.force or False
 
         if options.debug:
             logging.basicConfig(level=logging.DEBUG)
@@ -44,14 +48,14 @@ class Manager(object):
 
         mode = options.mode or 'update'
         project = args[1] if len(args) > 1 else DEFAULT_PROJECT_FILENAME
-        getattr(self, mode)(project)
+        getattr(self, mode)(project, force=force)
         
     def __get_project_filename(self, project):
         if project.find('.') == -1:
             project = '/'.join((project.rstrip('/ '), DEFAULT_PROJECT_FILENAME))
         return project, os.path.exists(project)               
         
-    def create(self, project):
+    def create(self, project, **kwargs):
         filename, exists = self.__get_project_filename(project)
         
         if exists:
@@ -89,33 +93,38 @@ default_compilation_level: simple         # possible values are: whitespace, sim
             except Exception, e:
                 die(e)
            
-    def update(self, project):
+    def update(self, project, **kwargs):
         filename, exists = self.__get_project_filename(project)
         if not exists:
             die("Project file doesn't exist: %s" % filename)
         with open(filename, 'r') as f:
             try:
-                p = JSCCProject(filename, yaml.load(f), compiler=self.compiler)
-                if p.is_valid(True):
+                force = kwargs.get('force', False)
+                logging.debug('Force: %s', force)
+                p = JSCCProject(filename, yaml.load(f), compiler=self.compiler, debug_mode=self.debug_mode)
+                if not force and p.is_valid(True):
                     print 'Project is up to date.'
                     return
                 else:
-                    p.make()
+                    p.make(force)
             except Exception, e:
                 die(e)
    
-    def watch(self, project):
+    def watch(self, project, **kwargs):
         filename, exists = self.__get_project_filename(project)
         if not exists:
             die("Project file doesn't exist: %s" % filename)
 
         with open(filename, 'r') as f:
             try:
-                p = JSCCProject(filename, yaml.load(f), compiler=self.compiler)
+                p = JSCCProject(filename, yaml.load(f), compiler=self.compiler, debug_mode=self.debug_mode)
             except Exception, e:
                 die(e)
 
         try:
+            if kwargs.get('force'):
+                p.make(True)
+            
             greetings = True
             respect_project_mtime = True
             while True:
